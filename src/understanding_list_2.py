9cf418945c7e54849ad831699d5cc6e0c26d3e38
esteban trejo <25303@upv.edu.mx>
2025-11-12 09:46:40 -0600

    feat understanding_list

---
+++ src/understanding_list_2.py
@@ -0,0 +1,12 @@
+# SLICING
+players = ["cr7" , 'messi' , 'Travis' , 'chicha' , 'corona']
+print(players[0:3]) 
+# slice es trabajar con un grupo especifico 
+# de elementos de una lista
+print(players[1:4]) #  ['messi' , 'Travis' , 'chicha']
+print(players[:4]) # players = ["cr7" , 'messi' , 'Travis' , 'chicha']
+print(players[2:]) 
+
+print(players[-3:])
+
+print(players[-3:-1])
---
+++ src/understanding_list_comprehension.py
@@ -0,0 +1,23 @@
+# List comprehensions
+
+"""
+Una list comprehension combina el for loop
+y la creacion de nuevos elementos en una 
+sola linea de codigo y tambien, automaticamente
+agrega el nuevo elemento en la lista, es decir,
+sin utilizar append
+"""
+
+Squeres=[value**2 for value in range(1,11)]
+print(Squeres)
+
+# Numeros pares con el range
+even_number_0_100 = list(range(0,101,2))
+print(even_number_0_100)
+
+# Numeros pares utilizando list comprehension
+evens_number_0_100 = [value for value in range(0,101)if value%2 > 0]
+print (f"evens list comprehension:", evens_list_compre)
+
+
+
