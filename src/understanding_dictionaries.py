bb9eb50b85f9395e0b0b31c93a1a9d0eeeed1fe7
esteban trejo <25303@upv.edu.mx>
2025-11-24 09:46:15 -0600

    tarea

---
+++ src/understanding_dictionaries.py
@@ -0,0 +1,55 @@
+# simple dictionary
+
+alien_0 = {'color': 'gren', 'points':5}
+
+# the simpliest dictionary
+alien_1 = {'color': 'yellow'}
+
+# Accessing values in a dictionary 
+print(alien_1)
+print(alien_0['points'])
+
+# Empty dictionary
+alien_2 ={}
+
+# modifying values in a dictionary
+alien_2 = {'color': 'yellow'}
+alien_2['color']= 'blue'
+
+# adding new key-value pairs
+alien_2['x_position']=0
+alien_2['y_position']=25
+
+print(alien_2)
+
+## Dictionary to store similar objects
+favorite_languajes ={
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+# print(f"sarah favorite language is {favorite_languajes['sarah']})
+
+# Looping through all key-value pairs
+
+
+
+# Looping through all keys and values separately
+for key in favorite_languajes.keys():
+    print(key)
+    
+    
+
+
+
+
+## TAREA
+    # Investigar:
+        # Nesting dictionaries
+        
+        # Listas de diccionario
+        # Listas en diccionario
+        # Diccionarios en diccionarios
+    
