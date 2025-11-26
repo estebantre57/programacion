be0d8404780cbe79fdcf1de4d892c3b1e5f4b62c
esteban trejo <25303@upv.edu.mx>
2025-11-26 09:42:07 -0600

    listas

---
+++ src/TAREAS_SEGUNDA_UNIDAD/2530344_EstebanTrejo_t_strings.py
@@ -0,0 +1,13 @@
+"""
+El string es un texto que va entre comillas. Los strings permiten 
+realizar operaciones básicas como obtener su longitud, convertir el 
+texto a mayúsculas o minúsculas y concatenar diferentes partes. Es 
+importante validar y normalizar la entrada del usuario para prevenir 
+errores y asegurar que el programa funcione correctamente.
+
+Este documento incluye seis problemas donde trabajaré con operaciones 
+de strings, como medir longitudes, cambiar mayúsculas y minúsculas, y 
+usar varios métodos integrados. Cada problema incluye entradas, salidas, 
+validaciones y tres casos de prueba para demostrar su funcionamiento.
+"""
+
---
+++ src/understanding_while_loop.py
@@ -0,0 +1,39 @@
+"""
+    doctring for understanding_while_loop_104
+    
+    utilizamos el while loop para ejecutar un bloque de codigo mientras 
+    una condicion sea verdadera
+    
+    Estructura basica del while loop en python 
+    
+        while condicion:
+            # bloque de codigo a ejecutar
+            
+    """
+    
+# Ejemplo basico de un while loop
+# Verificar si un numero esta en un
+# rango especifico (10 y entre 20)
+
+while True: # while loop infinito
+    try:
+        number = int(input("ingrese un numero entre 10 y 20:"))
+    
+        if number < 20 and number > 10:
+            print("Numero dentro de el rango,felicitaciones!")
+            break
+        else:
+            print("Numero fuera de el rango, intentalo de nuevo!")
+    except ValueError:
+        print("Entrada invalida, por favor ingrese un numero entero")
+    except KeyboardInterrupt:
+        print("\nPrograma terminado por el usuario.")
+              
+        
+        
+        
+        
+        
+        
+        
+        
